e92d707bf3f29a1088f93060091b780db76aa67a
yousi20162004 <fgerxt007@yeah.net>
2025-11-10 16:49:32 +0800

    Add initial code for method update.

--- source/textrecords/textrecords.d
+++ source/textrecords/textrecords.d
@@ -259,6 +259,17 @@ struct TextRecords(T)
 		return find!(S, recordField)(value, 0);
 	}
 
+	void update(S, string recordField, alias predicate)(const S value, size_t amount = 1)
+	{
+		foreach(ref record; recordArray_)
+		{
+			if(predicate(record))
+			{
+				mixin("record." ~ recordField ~ " = " ~ "value;");
+			}
+		}
+	}
+
 	/**
 			Removes a record(s).
 
@@ -628,4 +639,9 @@ unittest
 
 	auto nickNameRecords = irrCollector.findAllByNickName("hikki");
 	assert(nickNameRecords[0].realName == "Utada Hikaru");
+
+	//irrCollector.update!(size_t, q{ (T data) => data.id == 122 })(333, 0);
+	//irrCollector.update!(size_t, (IrregularNames data) => data.id == 122 && data.nickName == "Liz")(333, 0);
+	irrCollector.update!(size_t, "id", (IrregularNames data) => data.id == 122 && data.nickName == "Liz")(333, 0);
+	irrCollector.dump();
 }
